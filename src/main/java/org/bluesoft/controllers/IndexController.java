5f3b85183e2dce1509c47999e8f9c915ad817d1e
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-29 17:16:32 +0200

    adding JPA Query method to CategoryRepository and UnitOfMeasureRepository

--- src/main/java/org/bluesoft/controllers/IndexController.java
+++ src/main/java/org/bluesoft/controllers/IndexController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
         return "index";
--- src/main/java/org/bluesoft/repositories/CategoryRepository.java
+++ src/main/java/org/bluesoft/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package org.bluesoft.repositories;
 import org.bluesoft.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
 }
