aa544cb1bcd9f0b491abdc0c57feb4ee247ab98b
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-10-07 17:59:54 +0200

    Added MVC tests and Integration test

--- src/main/java/org/bluesoft/services/RecipeServiceForH2.java
+++ src/main/java/org/bluesoft/services/RecipeServiceForH2.java
@@ -1,5 +1,6 @@
 package org.bluesoft.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bluesoft.domain.Recipe;
 import org.bluesoft.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
@@ -7,17 +8,20 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
+public
 class RecipeServiceForH2 implements RecipeService {
 
     private final RecipeRepository recipeRepository;
 
-    RecipeServiceForH2(final RecipeRepository recipeRepository) {
+    public RecipeServiceForH2(final RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.info("Get all recipes...");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
---
+++ src/test/java/org/bluesoft/services/RecipeServiceForH2Test.java
@@ -0,0 +1,42 @@
+package org.bluesoft.services;
+
+import org.bluesoft.domain.Recipe;
+import org.bluesoft.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceForH2Test {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceForH2(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeHashSet = new HashSet<>();
+        recipeHashSet.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeHashSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(),1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
